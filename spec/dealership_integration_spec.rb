e156f749377ba2c76dcb4aa197df47d01e26723c
jin camou and Steven Galvin <jin@>
2017-04-26 11:36:09 -0700

    add and pass integration spec for adding a new vehicle

--- spec/dealership_integration_spec.rb
+++ spec/dealership_integration_spec.rb
@@ -24,6 +24,16 @@ describe('the dealership path', {:type =>:feature}) do
     expect(page).to have_content("Here are all the cars in this dealership")
   end
 
+  it('allows user to add a new vehicle') do
+    visit('/dealership/1')
+    click_on("Add a new vehicle")
+    fill_in("make", :with => "Toyota")
+    fill_in("model", :with => "Camry")
+    fill_in("year", :with => "1998")
+    click_on('Add Vehicle')
+    expect(page).to have_content("Success!")
+  end
+
 
 
 end
